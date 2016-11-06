d3888e49dea322b0f6744d8aa1e353bc0e65e754
Aaron <aaronmcavinue@gmail.com>
2016-11-06 20:33:52 +0000

    Import files from local machine.

---
+++ hello_server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+require('http');
+
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send("Hello World");
+});
+
+app.get('/version', function(request, response) {
+    response.send(request.httpVersion);
+});
+
+app.get('/headers', function(request, response) {
+    response.send(request.headers);
+});
+
+app.get('/headers/:header_name', function(request, response) {
+    response.send(request.headers[request.params.header_name]);
+});
+
+app.get('/:firstname/:lastname', function(request, response) {
+    var first = request.params.firstname;
+    var last = request.params.lastname;
+    response.send(["Hello", first, last].join(" "));
+});
+
+app.listen(process.env.PORT || 8080, process.env.IP);
